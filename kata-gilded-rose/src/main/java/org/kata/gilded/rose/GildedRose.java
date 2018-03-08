3162c6ae4904236688793ef38591bb25cace3625
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-03-08 11:52:18 +0800

    enum implementation

--- kata-gilded-rose/src/main/java/org/kata/gilded/rose/GildedRose.java
+++ kata-gilded-rose/src/main/java/org/kata/gilded/rose/GildedRose.java
@@ -12,53 +12,100 @@ class GildedRose {
         this.items = items;
     }
 
+    enum DegradeStrategy {
+        NON_DEGRADE() {
+            @Override
+            int getDegradationAmount() {
+                return 0;
+            }
+        },
+        DEFAULT_DEGRADE() {
+            @Override
+            int getDegradationAmount() {
+                boolean isSellInDaysOver = sellIn < 0;
+                int degradeBy = 1;
+
+                if (isSellInDaysOver) {
+                    degradeBy *= 2;
+                }
+                return degradeBy;
+            }
+        },
+        COUNTER_DEGRADE() {
+            @Override
+            int getDegradationAmount() {
+                boolean isSellInDaysOver = sellIn < 0;
+                int upgradeBy = 1;
+
+                if (isSellInDaysOver) {
+                    upgradeBy *= 2;
+                }
+                return -upgradeBy;
+            }
+        },
+        TICKET() {
+            @Override
+            int getDegradationAmount() {
+                boolean fiveMoreDaysToShow = sellIn < 5;
+                boolean tenMoreDaysToShow = sellIn < 10;
+                boolean showIsOver = sellIn < 0;
+
+                int upgradeBy = 1;
+
+                if (showIsOver)
+                    upgradeBy = Integer.MIN_VALUE;
+                else if (fiveMoreDaysToShow)
+                    upgradeBy = 3;
+                else if (tenMoreDaysToShow) {
+                    upgradeBy = 2;
+                }
+
+                return -upgradeBy;
+
+            }
+        };
+
+        protected int sellIn;
+
+        abstract int getDegradationAmount();
+
+        static DegradeStrategy to(Item item) {
+            DegradeStrategy strategy = DegradeStrategy.DEFAULT_DEGRADE;
+            switch (item.name) {
+            case SULFURAS:
+                strategy = DegradeStrategy.NON_DEGRADE;
+                break;
+            case BACKSTAGE_PASSES:
+                strategy = DegradeStrategy.TICKET;
+                break;
+            case AGED_BRIE:
+                strategy = DegradeStrategy.COUNTER_DEGRADE;
+                break;
+            default:
+                strategy = DegradeStrategy.DEFAULT_DEGRADE;
+            }
+
+       
+            return strategy;
+        }
+        
+        public void setSellIn(int sellIn) {
+            this.sellIn = sellIn;
+        }
+
+    }
+
     public void updateQuality() {
         for (Item item : items) {
 
-            String name = item.name;
-
-            boolean isAgedBrie = name.equals(AGED_BRIE);
-            boolean isBackStagePasses = name.equals(BACKSTAGE_PASSES);
-            boolean isSulfuras = name.equals(SULFURAS);
-
-            if (isSulfuras)
+            DegradeStrategy strategy = DegradeStrategy.to(item);
+            
+            if (strategy == DegradeStrategy.NON_DEGRADE)
                 continue;
-
-            --item.sellIn;
             
-            boolean isSellInDaysOver = item.sellIn < 0;
-            boolean isQualityDegradingItem = !(isAgedBrie || isBackStagePasses);
-            boolean isTicket = isBackStagePasses;
+            strategy.setSellIn(--item.sellIn);
             
-            int decrementBy = 1;
-
-            if (isSellInDaysOver) {
-                decrementBy *= 2;
-            }
-
-            if (isTicket) {
-
-                boolean lessThan5DaysToShow = item.sellIn < 5;
-                boolean lessThan10DaysToShow = item.sellIn < 10;
-                boolean showIsOver = isSellInDaysOver;
-
-                if (showIsOver)
-                    decrementBy = -item.quality;
-                else if (lessThan5DaysToShow)
-                    decrementBy = 3;
-                else if (lessThan10DaysToShow) {
-                    decrementBy = 2;
-                } else {
-                    decrementBy = 1;
-                }
-
-            }
-            
-            if (!isQualityDegradingItem) {
-                decrementBy = -decrementBy;
-            }
-
-            decreaseQuantity(item, decrementBy);
+            decreaseQuantity(item, strategy.getDegradationAmount());
 
         }
     }
