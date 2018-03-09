40e79ca571fcf96b83dda1fb970093f6b1572a1c
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-03-09 12:32:11 +0800

    5 statement -> statement

--- transformation-priority-premise/src/test/java/org/transformation/priority/premise/CountryFromCodeKata.java
+++ transformation-priority-premise/src/test/java/org/transformation/priority/premise/CountryFromCodeKata.java
@@ -3,6 +3,9 @@ package org.transformation.priority.premise;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
 public class CountryFromCodeKata {
@@ -16,14 +19,17 @@ public class CountryFromCodeKata {
         verifyCountryAndCode("Australia", "AU");
         verifyCountryAndCode("Canada", "CA");
         verifyCountryAndCode("Sri Lanka", "SL");
+        verifyCountryAndCode("Viet Nam", "VN");
         
     }
 
     private String getCountryCode(String country) {
-        if(country.equals("Sri Lanka")) {
-            return "SL";
+        String code = "";
+        if(country.matches(".* .*")) {
+            code = Arrays.stream(country.split(" ")).map(s -> s.substring(0, 1)).collect(Collectors.joining());
+        } else {            
+            code = country.substring(0,2);
         }
-        String code = country.substring(0,2);
         code = code.toUpperCase();
         return code;
     }
