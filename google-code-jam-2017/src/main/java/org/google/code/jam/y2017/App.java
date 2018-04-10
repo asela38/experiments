f2baf6a0ced0522566caa001951f3813b8e6056e
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-04-10 13:14:25 +0800

    google code jam 2017 solutions

---
+++ google-code-jam-2017/src/main/java/org/google/code/jam/y2017/App.java
@@ -0,0 +1,13 @@
+package org.google.code.jam.y2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
