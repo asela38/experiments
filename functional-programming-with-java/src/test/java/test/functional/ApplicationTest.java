51fa72c1e598bc3368b1b62bebfdb7984adabd8b
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-07-06 09:23:44 +0800

    Create a project to house functional training

---
+++ functional-programming-with-java/src/test/java/test/functional/ApplicationTest.java
@@ -0,0 +1,12 @@
+package test.functional;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void trivial() throws Exception {
+        System.out.println("Hello, World");
+    }
+
+}
