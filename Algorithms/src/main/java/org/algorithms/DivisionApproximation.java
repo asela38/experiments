ce09ab9bed9ee592e04d472b94352ca8d84ca201
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-07-23 13:32:27 +0800

    Few examples

---
+++ Algorithms/src/main/java/org/algorithms/DivisionApproximation.java
@@ -0,0 +1,15 @@
+package org.algorithms;
+
+import org.junit.Test;
+
+public class DivisionApproximation {
+
+    
+    @Test
+    public void divideBy3Test() throws Exception {
+        for(int i = 0; i < 1000; i++) {
+            System.out.println( ( i / 3) - ( ( i >> 1 ) + ( i >> 2) + (i >> 3 ) + (i >> 4) + (i >> 5)) );
+        }
+        
+    }
+}
