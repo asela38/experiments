6af54d544a735db2dc9721451fc906f728a233dd
asela.illayapparachc <asela.illayapparachc@gmail.com>
2018-07-23 13:34:25 +0800

    Some examples

---
+++ Algorithms/src/main/java/org/algorithms/KargerMinCutExploration.java
@@ -0,0 +1,98 @@
+package org.algorithms;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class KargerMinCutExploration {
+
+	private static final String FILE_PATH_1 = "/Users/asela.illayapparachc/Documents/algorithms/kargerMinCut.txt";
+
+	@Test
+	public void testRepresentation() throws Exception {
+		List<String> lines = Files.readAllLines(new File(FILE_PATH_1).toPath());
+		Map<Integer, List<Integer>> map = lines.stream().map(s -> s.split("\\s"))
+				.collect(Collectors.toMap(a -> Integer.valueOf(a[0]),
+						a -> Arrays.stream(a).skip(1).map(Integer::valueOf).collect(Collectors.toList())));
+		System.out.println(map);
+
+		int n = map.keySet().size();
+		System.out.println("Number of Nodes :  " + n);
+
+		Set<String> vertextSet2 = map.entrySet().stream()
+				.flatMap(e -> e.getValue().stream().map(a -> new int[] { e.getKey(), a.intValue() }))
+				.map(a -> Integer.max(a[0], a[1]) + "-" + Integer.min(a[0], a[1])).distinct()
+				.collect(Collectors.toSet());
+
+		System.out.println(IntStream.range(0, 400).map(ii -> {
+			List<int[]> vertextSet = vertextSet2.stream().map(a -> a.split("-"))
+					.map(a -> new int[] { Integer.parseInt(a[0]), Integer.parseInt(a[1]) })
+					.collect(Collectors.toList());
+
+			// System.out.println(vertextSet.size());
+			vertextSet.stream().filter(a -> a[0] == a[1]).map(Arrays::toString).forEach(System.out::println);
+			vertextSet = vertextSet.stream().filter(a -> a[0] != a[1]).collect(Collectors.toList());
+			// System.out.println(vertextSet.size());
+
+			for (int i = 0; i < n - 2; i++) {
+				int[] first = vertextSet.remove(ThreadLocalRandom.current().nextInt(0, vertextSet.size()));
+				vertextSet = vertextSet.stream().map(a -> {
+					if (a[0] == first[0]) {
+						a[0] = first[1];
+					}
+
+					if (a[1] == first[0]) {
+						a[1] = first[1];
+					}
+					return a;
+				}).collect(Collectors.toList());
+
+				// System.out.println(vertextSet.size());
+				// vertextSet.stream().filter(a -> a[0] ==
+				// a[1]).map(Arrays::toString).forEach(System.out::println);
+				vertextSet = vertextSet.stream().filter(a -> a[0] != a[1]).collect(Collectors.toList());
+			}
+
+			// System.out.println(vertextSet.size());
+			//
+
+			int size = vertextSet.size();
+			if (size < 18) {
+				vertextSet.stream().map(Arrays::toString).map(s -> s + " ").forEach(System.out::print);
+			}
+			System.out.println(ii + " -> " + size);
+			return size;
+		}).summaryStatistics());
+	}
+
+	private void firstAttempt_FailedSinceExponentialGrowth(Map<Integer, List<Integer>> map, int n) {
+		Set<Integer> rSet = new HashSet<>();
+
+		for (int i = 1; i <= n - 2; i++) {
+			System.out.println(i);
+			List<Integer> list = map.remove(i);
+			rSet.add(i);
+			list.removeAll(rSet);
+			System.out.println(i + " -> " + list.size());
+			for (Integer j : list) {
+				map.get(j).addAll(list);
+				map.get(j).remove(j);
+				map.get(j).remove(Integer.valueOf(i));
+			}
+
+		}
+
+		System.out.println(map);
+		System.out.println(map.size());
+	}
+
+}
---
+++ Algorithms/src/main/java/org/algorithms/QuickSortOriginal2.java
@@ -0,0 +1,106 @@
+package org.algorithms;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.Test;
+
+public class QuickSortOriginal2 {
+
+	private static final String FILE_PATH_1 = "/Users/asela.illayapparachc/Documents/algorithms/QuickSort.txt";
+
+	@Test
+	public void testQuickSort() throws Exception {
+		int multiplier = 1000;
+		int[] array1 = ThreadLocalRandom.current().ints(0, 20 * multiplier).limit(20 * multiplier).toArray();
+		int[] array2 = Arrays.copyOf(array1, array1.length);
+		quickSort(array1);
+		Arrays.sort(array2);
+		assertEquals(Arrays.toString(array1), Arrays.toString(array2));
+	}
+
+	@Test
+	public void testWithFile() throws Exception {
+		int[] ints = readNumbers();
+		System.out.println(Arrays.stream(ints).summaryStatistics());
+		System.out.printf("%n%s-%s-%s%n", ints[0], ints[ints.length / 2], ints[ints.length - 1]);
+		a = 0;
+		quickSort(ints);
+		System.out.println(a);
+
+		a = 0;
+		ints = readNumbers();
+		quickSort(ints);
+		System.out.println(a);
+
+		a = 0;
+		ints = readNumbers();
+		quickSort(ints);
+		System.out.println(a);
+	}
+
+	private int[] readNumbers() throws IOException {
+		return Files.readAllLines(new File(FILE_PATH_1).toPath()).stream().mapToInt(Integer::parseInt).toArray();
+	}
+
+	private void quickSort(int[] array) {
+		quickSortOnRange(array, 0, array.length - 1);
+	}
+
+	int a = 0;
+
+	private int[] quickSortOnRange(int[] arr, int from, int to) {
+
+		if (to <= from)
+			return arr;
+
+		int point = partitionOnRange(arr, from, to);
+		quickSortOnRange(arr, from, point - 1);
+		quickSortOnRange(arr, point + 1, to);
+
+		return arr;
+	}
+
+	private int partitionOnRange(int[] arr, int from, int to) {
+		if (to <= from)
+			return to;
+
+		int mid = (from + to) >> 1;
+		if (isBetween(arr, from, to, mid))
+			swap(arr, from, mid);
+		if (isBetween(arr, from, mid, to))
+			swap(arr, from, to);
+
+		if (false)
+			swap(arr, from, to);
+
+		int pivot = arr[from];
+		int index = from + 1;
+
+		a += to - from;
+		for (int i = from + 1; i <= to; i++)
+			if (arr[i] < pivot) {
+				swap(arr, index++, i);
+			}
+
+		swap(arr, --index, from);
+
+		return index;
+	}
+
+	private boolean isBetween(int[] arr, int from, int to, int test) {
+		return (arr[test] > arr[from] && arr[test] < arr[to]) || (arr[test] < arr[from] && arr[test] > arr[to]);
+	}
+
+	private void swap(int[] arr, int from, int to) {
+		int temporary = arr[from];
+		arr[from] = arr[to];
+		arr[to] = temporary;
+	}
+
+}
