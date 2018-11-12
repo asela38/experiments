a06c78861e918671906b5aa46d44901967480f19
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-11-12 10:01:13 +0800

    Code for NAND to TETTRIS assembler

---
+++ nand-to-tettris/src/main/java/org/nand/to/tettris/HackAssembler.java
@@ -0,0 +1,8 @@
+package org.nand.to.tettris;
+
+/** Hello world! */
+public class HackAssembler {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ nand-to-tettris/src/test/java/org/nand/to/tettris/HackAssemblerTest.java
@@ -0,0 +1,12 @@
+package org.nand.to.tettris;
+
+import org.junit.Test;
+
+public class HackAssemblerTest {
+
+    @Test
+    public void verify() throws Exception {
+        System.out.println("Hello, World");
+
+    }
+}
