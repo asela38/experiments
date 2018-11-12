7620f3fc33e736c4092de2bc244e087e73d283b3
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-11-12 10:47:27 +0800

    code to fileter comment and empty lines

--- nand-to-tettris/src/main/java/org/nand/to/tettris/HackAssembler.java
+++ nand-to-tettris/src/main/java/org/nand/to/tettris/HackAssembler.java
@@ -1,8 +1,38 @@
 package org.nand.to.tettris;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 /** Hello world! */
 public class HackAssembler {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        if (args == null || args.length < 1)
+            throw new IllegalArgumentException("File Name should be specified");
+
+        List<String> readAllLines = getAsmLines(args);
+
+        // clean empty lines and comments
+        readAllLines.stream()
+                .map(line -> line.replaceAll("\\s*", "")) // remove all spaces
+                .filter(line -> !line.isEmpty())
+                .filter(line -> !line.startsWith("//"))
+                .map(line -> line.replace("//.*", ""))
+                .forEach(System.out::println);
+
+    }
+
+    private static List<String> getAsmLines(String[] args) {
+        File file = new File(args[0]);
+        try {
+            return Files.readAllLines(Paths.get(file.toURI()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 }
--- nand-to-tettris/src/test/java/org/nand/to/tettris/HackAssemblerTest.java
+++ nand-to-tettris/src/test/java/org/nand/to/tettris/HackAssemblerTest.java
@@ -1,12 +1,35 @@
 package org.nand.to.tettris;
 
+import java.io.File;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class HackAssemblerTest {
 
     @Test
-    public void verify() throws Exception {
-        System.out.println("Hello, World");
+    public void verify_execution() throws Exception {
+        System.out.printf("File loaded form : %s%n", new File(".").getAbsolutePath());
+        Process process = Runtime.getRuntime()
+                .exec(String.format(
+                        "java -cp target/classes org.nand.to.tettris.HackAssembler target/test-classes/%s", "Add.asm"));
+        System.out.printf("Wait for the process to complete. Result: %s%n", process.waitFor());
+
+        System.out.println("--Inputstream:");
+        try (Scanner scanner = new Scanner(process.getInputStream())) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+
+        }
+
+        System.out.println("--Errorstream:");
+        try (Scanner scanner = new Scanner(process.getErrorStream())) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+
+        }
 
     }
 }
