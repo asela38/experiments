5960927adf92b4261ff821f36ce1cba441bfbd8c
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-12-28 18:04:00 +0800

    fix few tags

--- nand-to-tettris/src/main/java/org/nand/to/tettris/compiler/analyser/CompilationEngine.java
+++ nand-to-tettris/src/main/java/org/nand/to/tettris/compiler/analyser/CompilationEngine.java
@@ -13,6 +13,12 @@ public class CompilationEngine implements Closeable {
         tokenizer = new JackTokenizer(inFile);
         writer = new PrintWriter(outFile);
 
+        // debugTokenizer();
+
+        compileClass();
+    }
+
+    private void debugTokenizer() {
         while (tokenizer.hasMoreTokens()) {
             tokenizer.advance();
             switch (tokenizer.tokenType()) {
@@ -40,12 +46,152 @@ public class CompilationEngine implements Closeable {
         }
     }
 
-    public void compileClass() {
+    private void openTag(String tag) {
+        writer.printf("<%s>%n", tag);
+    }
 
+    private void closeTag(String tag) {
+        writer.printf("</%s>%n", tag);
+    }
+
+    private void singleTag(String key, String value) {
+        writer.printf("<%1$s>%2$s</%1$s>%n", key, value);
+    }
+
+    private void keyWordTag() {
+        singleTag("keyword", tokenizer.keyWord().name().toLowerCase());
+        tokenizer.advance();
+    }
+
+    private void identifierTag() {
+        singleTag("identifier", tokenizer.identifier());
+        tokenizer.advance();
+    }
+
+    private void symbolTag() {
+        singleTag("symbol", String.valueOf(tokenizer.symbol()));
+        tokenizer.advance();
+    }
+
+    public void compileClass() {
+        tokenizer.advance();
+        openTag("class");
+
+        keyWordTag();
+        identifierTag();
+        symbolTag();
+
+        outer: while (tokenizer.tokenType() == TokenType.KEYWORD) {
+            switch (tokenizer.keyWord()) {
+
+            case STATIC:
+            case FIELD:
+                compileClassVarDec();
+                break;
+            case FUNCTION:
+            case METHOD:
+            case CONSTRUCTOR:
+                compileSubroutineDec();
+                break;
+            default:
+                break outer;
+            }
+        }
+
+        symbolTag();
+        closeTag("class");
     }
 
     public void compileClassVarDec() {
 
+    }
+
+    public void compileSubroutineDec() {
+        openTag("subroutineDec");
+        keyWordTag();
+        keyWordTag();
+        identifierTag();
+        symbolTag();
+        compileParameterList();
+        symbolTag();
+        compileSubroutineBody();
+        closeTag("subroutineDec");
+
+    }
+
+    public void compileParameterList() {
+        openTag("parameterList");
+        closeTag("parameterList");
+
+    }
+
+    public void compileSubroutineBody() {
+        openTag("subroutineBody");
+        symbolTag();
+        compileVarDec();
+        closeTag("subroutineBody");
+    }
+
+    public void compileVarDec() {
+        while (tokenizer.keyWord() == KeyWord.VAR) {
+
+            openTag("varDec");
+            keyWordTag();
+            identifierTag();
+            identifierTag();
+            while (tokenizer.symbol() == ',') {
+                symbolTag();
+                identifierTag();
+            }
+            symbolTag();
+            closeTag("varDec");
+        }
+
+    }
+
+    public void compileStatements() {
+        openTag("statements");
+        closeTag("statements");
+    }
+
+    public void compileLet() {
+        openTag("letStatement");
+        closeTag("letStatement");
+    }
+
+    public void compileIf() {
+        openTag("ifStatement");
+        closeTag("ifStatement");
+    }
+
+    public void compileWhile() {
+        openTag("whileStatement");
+        closeTag("whileStatement");
+    }
+
+    public void compileDo() {
+        openTag("doStatement");
+        closeTag("doStatement");
+    }
+
+    public void compileReturn() {
+        openTag("returnStatement");
+        closeTag("returnStatement");
+    }
+
+    public void compileExpression() {
+        openTag("expression");
+        closeTag("expression");
+    }
+
+    public void compileTerm() {
+        openTag("term");
+        closeTag("term");
+    }
+
+    public void compileExpressionList() {
+        openTag("expressionList");
+        closeTag("expressionList");
     }
 
     @Override
