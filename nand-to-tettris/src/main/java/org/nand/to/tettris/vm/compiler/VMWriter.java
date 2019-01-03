89dcae66fbca1e7fcafa47ef941bba19105f299b
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2019-01-03 12:47:20 +0800

    WMWriter implementation

--- nand-to-tettris/src/main/java/org/nand/to/tettris/vm/compiler/VMWriter.java
+++ nand-to-tettris/src/main/java/org/nand/to/tettris/vm/compiler/VMWriter.java
@@ -1,5 +1,88 @@
 package org.nand.to.tettris.vm.compiler;
 
-public class VMWriter {
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class VMWriter implements Closeable {
+
+    public enum Segment {
+        CONST("constant"), ARG("argument"), LOCAL("local"), STATIC("static"), THIS("this"), THAT("that"), POINTER(
+                "pointer"), TEMP("temp");
+
+        private String name;
+
+        private Segment(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public enum Command {
+        ADD("add"), SUB("sub"), NEG("neg"), EQ("eq"), GT("gt"), LT("lt"), AND(
+                "and"), OR("or"), NOT("not");
+
+        private String name;
+
+        private Command(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private PrintWriter writer;
+
+    public VMWriter(File vmFile) throws Exception {
+        writer = new PrintWriter(vmFile);
+    }
+
+    public void writePush(Segment segment, int index) {
+        writer.printf("push %s %d %n", segment.getName(), index);
+    }
+
+    public void writePop(Segment segment, int index) {
+        writer.printf("pop %s %d %n", segment.getName(), index);
+    }
+
+    public void writeArithmetic(Command command) {
+        writer.printf("%s %n", command.getName());
+    }
+
+    public void writeLabel(String label) {
+        writer.printf("(%s) %n", label);
+    }
+
+    public void writeIf(String label) {
+        writer.printf("if-goto %s %n", label);
+    }
+
+    public void writeGoto(String label) {
+        writer.printf("goto %s %n", label);
+    }
+
+    public void writeCall(String name, int nArgs) {
+        writer.printf("call %s %d %n", name, nArgs);
+    }
+
+    public void writeFunction(String name, int nLocals) {
+        writer.printf("call %s %d %n", name, nLocals);
+    }
+
+    public void writeReturn() {
+        writer.printf("return %n");
+    }
+
+    @Override
+    public void close() throws IOException {
+        writer.flush();
+        writer.close();
+    }
 
 }
